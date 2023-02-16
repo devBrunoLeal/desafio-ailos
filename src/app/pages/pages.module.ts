ef3574d47427c73371ae485072f696841bb0b453
brunonovato22 <brunonovato22@gmail.com>
2023-02-16 16:01:31 -0300

    Removendo imports desnecessários

--- src/app/pages/pages.module.ts
+++ src/app/pages/pages.module.ts
@@ -1,10 +1,6 @@
-import { NovaAdmissaoComponent } from './nova-admissao/nova-admissao.component';
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { FavoritosComponent } from './favoritos/favoritos.component';
-import { NgxMaskModule } from 'ngx-mask';
-import { ReactiveFormsModule } from '@angular/forms';
-import {MatProgressSpinnerModule} from '@angular/material/progress-spinner';
 import { NovaAdmissaoModule } from './nova-admissao/nova-admissao.module';
 
 @NgModule({
